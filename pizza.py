9c9ff64846a5452fa3e03f212ed00db94587361a
RanjethaSubash <71769294+RanjethaSubash@users.noreply.github.com>
2020-09-25 00:01:02 +0530

    Update pizza.py

--- pizza.py
+++ pizza.py
@@ -1,7 +1,5 @@
-from flask import Flask,request,jsonify,render_template
+from flask import Flask,request,render_template
 import MysqlConnection as connect
-import Mysqlresponse as response
-import Mysqlerror as er
 app = Flask(__name__)
 @app.route("/")
 def index():
@@ -9,6 +7,7 @@ def index():
 
 @app.route("/webhook", methods=['POST','GET'])
 def webhook():
+    print("hi")
     req = request.get_json(silent=True, force=True)
     temp = insert(req)
     return temp
@@ -28,10 +27,10 @@ def insert(req):
                         }]
                     }
                 }]
-
         }
     else:
         try:
+            print("hello")
             pizza_name = req.get('queryResult').get('parameters').get('pizza_name')
             pizza_size = req.get('queryResult').get('parameters').get('pizza_size')
             crust_pizza = req.get('queryResult').get('parameters').get("crust_pizza")
@@ -40,20 +39,21 @@ def insert(req):
             cust_name  = req.get('queryResult').get('parameters').get("cust_name")['name']
             phone_no = req.get('queryResult').get('parameters').get("phone_no")
             mail_id = req.get('queryResult').get('parameters').get("mail_id")
-            address =req.get('queryResult').get('parameters').get("address")
-            status = req.get('queryResult').get('parameters').get("confirmation")
+            address = req.get('queryResult').get('parameters').get("address")
+            status = req.get('queryResult').get('parameters').get("confirm")
             payment = req.get('queryResult').get('parameters').get("payment")
             amt = int(int(req.get('queryResult').get('parameters').get("count_pizza"))*230)
             cur_con = con.cursor()
-            print(amt)
-            print(phone_no)
-            print(topping)
-            query = "insert into pizza_orders_info(pizza_name,pizza_size,crust_pizza,pizza_toppings,pizza_count,cust_name,phone_no,mail_id,address,status,payment,amt) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"
-            val = (pizza_name, pizza_size, crust_pizza, topping, count_pizza, cust_name, phone_no, mail_id, address, status,payment, amt)
-            cur_con.execute(query, val)
-            con.commit()
-            order_id = cur_con.lastrowid
-            speech = "Your order has been recorded successfully! <br/> Order_id : " + str(order_id) + "<br/> Name : " + cust_name + "<br/> Ordered Pizza : " + str(pizza_name) + " " + pizza_size + " " + "<br/> Amount : " + str(amt) + "<br/> status : ordered."
+            if (status=="yes"):
+                query = "insert into pizza_orders(pizza_name,pizza_size,crust_pizza,pizza_toppings,pizza_count,cust_name,phone_no,mail_id,address,payment,amt) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"
+                val = (pizza_name, pizza_size, crust_pizza, topping, count_pizza, cust_name, phone_no, mail_id, address,payment,amt)
+                cur_con.execute(query, val)
+                con.commit()
+                order_id = cur_con.lastrowid
+                print(order_id)
+                speech = "Your order has been recorded successfully! <br/> Order_id : " + str(order_id) + "<br/> Name : " + cust_name + "<br/> Ordered Pizza : " + str(pizza_name) + " " + pizza_size + " " + "<br/> Amount : " + str(amt) + "<br/> status : ordered."
+            else:
+                speech = "Your order has been cancelled!"
             return {
                 "fulfillmentText": speech,
                 "fulfillmentMessages": [
@@ -67,6 +67,7 @@ def insert(req):
                     }]
             }
         except Exception as e:
+            print(e)
             speech = "Some error has occurred!"
             return {
                 "fulfillmentText": speech,
@@ -81,9 +82,5 @@ def insert(req):
                     }]
 
             }
-
-
-
-
 if __name__ == "__main__":
     app.run(debug=True,port = 80)
