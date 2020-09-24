c5bfed70b67841749b9ae8f1fcab2c3c60d04e14
RanjethaSubash <71769294+RanjethaSubash@users.noreply.github.com>
2020-09-24 17:46:38 +0530

    Update MysqlConnection.py

--- MysqlConnection.py
+++ MysqlConnection.py
@@ -3,7 +3,7 @@ from mysql.connector import connect, errorcode, Error
 
 def connect_mysql():
     try:
-        database_connect = connect(host="localhost", user="root", password="Pwd$4mysql520", database="summa")
+        database_connect = connect(host="localhost", user="root", password="Pwd$4mysql520", database="pizzaBot")
         return database_connect
     except Error as er:
         if er.errno == errorcode.ER_ACCESS_DENIED_ERROR:
